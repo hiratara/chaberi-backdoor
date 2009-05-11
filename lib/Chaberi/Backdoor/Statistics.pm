c8579aff5b191215d490b9b4674dbeed00ac7229
hiratara <hiratara@cpan.org>
2009-05-11 23:52:46 +0900

    all data into the page reference.

--- lib/Chaberi/Backdoor/Statistics.pm
+++ lib/Chaberi/Backdoor/Statistics.pm
@@ -19,11 +19,26 @@ has page => (
 );
 
 
+has schema => (
+	isa     => 'Chaberi::Backdoor::Schema',
+	is      => 'ro',
+	default => sub { Chaberi::Backdoor::Schema->default_schema },
+);
+
+
+has now_epoch => (
+	isa     => 'Int',
+	is      => 'ro',
+	default => sub { scalar time },
+);
+
+
 # subroutine =====================================
 sub _calc_range{
-	my ($schema, $room, $nick, $epoch) = @_;
+	my $self = shift;
+	my ($room, $nick) = @_;
 
-	my $rs = $schema->resultset('EnterRange');
+	my $rs = $self->schema->resultset('EnterRange');
 
 	my $cur_epoch = $rs->search(
 		{
@@ -31,7 +46,7 @@ sub _calc_range{
 			nick_id  => $nick->id,
 		}, 
 	)->get_column('epoch2')->max;
-	if ( $cur_epoch and $epoch - $cur_epoch < $MARGINE ) {
+	if ( $cur_epoch and $self->now_epoch - $cur_epoch < $MARGINE ) {
 		# 前回の範囲を拡張する
 		my $range = $rs->search(
 			{
@@ -40,7 +55,7 @@ sub _calc_range{
 				epoch2	 => $cur_epoch,
 			}, 
 		)->first;
-		$range->epoch2($epoch);
+		$range->epoch2($self->now_epoch);
 		$range->update;
 		return $range;
 	} else {
@@ -49,10 +64,38 @@ sub _calc_range{
 			{
 				room_id => $room->id,
 				nick_id => $nick->id,
-				epoch1 => $epoch,
-				epoch2 => $epoch,
+				epoch1 => $self->now_epoch,
+				epoch2 => $self->now_epoch,
 			}
 		);
+	}
+}
+
+
+# merge dbdata into page data (i.e. change page field destructively.)
+sub _merge_statistics{
+	my $self = shift;
+
+	# load DB data
+	for my $room_info ( @{ $self->page->{rooms} }){
+		my $room = $self->schema->resultset('Room')->find({
+			unique_key => $room_info->{link},
+		});
+
+		next unless $room;
+
+		$room_info->{obj_room} = $room;
+
+		for my $member ( $room_info->{status}->all_members ){
+			my $nick = $self->schema->resultset('Nick')->find_or_new(
+				name => $member->name,
+			)->insert();
+			my $range = $self->_calc_range($room, $nick);
+
+			# XXX $member is object so this code is too agly.
+			$member->{obj_nick}  = $nick;
+			$member->{obj_range} = $range;
+		}
 	}
 }
 
@@ -64,26 +107,7 @@ sub START {}
 event exec => sub {
 	my ($self) = @_[OBJECT, ARG0 .. $#_];
 
-	my $schema = Chaberi::Backdoor::Schema->default_schema;
-
-	# load DB data
-	for my $room_info ( @{ $self->page->{rooms} }){
-		my $room = $schema->resultset('Room')->find({
-			unique_key => $room_info->{link},
-		});
-		for my $member ( $room_info->{status}->all_members ){
-#	my $nick = $_schema->resultset('Nick')->find_or_new(
-#		name => normalize_nick($name_str),
-#	)->insert();
-			my $nick = $schema->resultset('Nick')->find({
-				name => $member->name,
-			});
-			if($nick and $room){
-				my $range = _calc_range($schema, $room, $nick, time);
-			}
-		}
-	}
-
+	$self->_merge_statistics;
 
 	$POE::Kernel::poe_kernel->post(
 		@{ $self->cont }, $self->page
