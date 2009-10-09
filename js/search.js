0e6d19bede46843a5b736cf0fa1c600c9bbef4d0
hiratara <hiratara@cpan.org>
2009-10-10 00:18:45 +0900

    Many kind of search.

--- js/search.js
+++ js/search.js
@@ -6,15 +6,24 @@ $( function () {
         $("#wait-anime").show();
 
         var str   = $( "#search" ).val();
-        var names = $(".member-names").get();
+        var match = $.map(
+            $( "input[name=match]:checked" ).get(),
+            function (elem, i) { return $( elem ).val(); }
+        );
 
+        var tr = $(".display-table tr").get();
         Deferred.loop(
-        { begin: 0, end: names.length, step: 20 }, function (n, o) {
-            for (var i = n; i < n + o.step && i < names.length; i++){
-                if( $( names[i] ).text().indexOf( str ) >= 0 ) 
-                    $( names[i] ).parent().show();
+        { begin: 0, end: tr.length, step: 20 }, function (n, o) {
+            for (var i = n; i < n + o.step && i < tr.length; i++){
+                var matched = false;
+                for(var j = 0; j < match.length; j++){
+                    if( $(match[j], tr[i]).text().indexOf( str ) >= 0 )
+                        matched = true;
+                }
+                if( matched )
+                    $( tr[i] ).show();
                 else
-                    $( names[i] ).parent().hide();
+                    $( tr[i] ).hide();
             }
         }).error(function (e) {
             alert(e);
