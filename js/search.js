366bfc8f29663241ffdbf79d0097a001544f9c68
namusyaka <namusyaka@gmail.com>
2011-01-20 06:36:50 -0800

    Enterで検索処理が開始するようにしました。

--- js/search.js
+++ js/search.js
@@ -1,31 +1,42 @@
 ( function ($) {
 
 $( function () {
-    $("#search-button").click( function () {
+    var search = function () {
         $("#search-button").attr("disabled", "disabled");
         $("#wait-anime").show();
 
-        var str   = $( "#search" ).val();
+        var str = $( "#search" ).val();
         var match = $.map(
             $( "input[name=match]:checked" ).get(),
             function (elem, i) { return $( elem ).val(); }
         );
-        var tr = $(".display-table tr").get();
 
-        for(var i = 1; i < tr.length; i++){
-          if($(tr[i]).text().indexOf(str)){
-            for(var j = 0; j < match.length; j++){
-              if($(match[j], tr[i]).text().indexOf( str ) >= 0 ){
-                $(tr[i]).show();
-                break
-              }
-              if(j === match.length-1)$(tr[i]).hide()
+        var tr = $(".display-table tr").get();
+        Deferred.loop(
+        { begin: 0, end: tr.length, step: 20 }, function (n, o) {
+            for (var i = n; i < n + o.step && i < tr.length; i++){
+                var matched = false;
+                for(var j = 0; j < match.length; j++){
+                    if( $(match[j], tr[i]).text().indexOf( str ) >= 0 )
+                        matched = true;
+                }
+                if( matched )
+                    $( tr[i] ).show();
+                else
+                    $( tr[i] ).hide();
             }
-          }else $(tr[i]).hide()
-        }
-        
-        $("#search-button").removeAttr("disabled");
-        $("#wait-anime").hide()
+        }).error(function (e) {
+            alert(e);
+        }).next(function (){
+            $("#wait-anime").hide();
+            $("#search-button").removeAttr("disabled");
+        });
+    }
+    $("#search-button").click( function(){search()});
+    $("#search").keypress(function(e) {
+      if((e || event).which == 13){
+        if($("#search").val()) search()
+      }
     });
 });
 
