4ed6eacf17d0afe68143980f2a70a4e384560ffd
Masahiro Honma <hiratara@cpan.org>
2011-09-03 23:29:17 +0900

    Run module-setup.

---
+++ lib/Chaberi/Backdoor.pm
@@ -0,0 +1,32 @@
+package Chaberi::Backdoor;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Chaberi::Backdoor -
+
+=head1 SYNOPSIS
+
+  use Chaberi::Backdoor;
+
+=head1 DESCRIPTION
+
+Chaberi::Backdoor is
+
+=head1 AUTHOR
+
+hiratara E<lt>hiratara {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
